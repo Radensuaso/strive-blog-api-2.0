a89d177e9f0127ddb4d405a8ad7f8fc43da34c58
André Sousa <66281967+Radensuaso@users.noreply.github.com>
2021-09-28 13:24:58 +0100

    Homework initialised

--- src/services/authors/schema.js
+++ src/services/authors/schema.js
@@ -5,6 +5,7 @@ const authorSchema = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true },
+    password: { type: String, required: true },
     birthDate: { type: Date, required: true },
     avatar: { type: String, required: true },
   },
